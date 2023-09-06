bbca3b816122cea87dd201b1b7bcf30a9357507c
isaacolanre <isaacolanre@gmail.com>
2023-09-06 11:45:50 +0100

    added ts-node package to project added defualt route

--- index.ts
+++ index.ts
@@ -1,31 +1,38 @@
-import express, { Application } from "express";
+import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import sequelize from "./config/database";
+import router from "./routes/school.routes";
+import paymentRouter from "./routes/payment.routes";
 
 // Load environment variables from .env file
 dotenv.config();
 
-import sequelize from "./config/database";
-import router from "./routes/school.routes";
-import paymentRouter from "./routes/payment.routes"
+const app = express();
+const port = process.env.PORT || 4401;
 
-const app: Application = express();
-
-
-
+// Middleware
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(router);
 app.use(paymentRouter);
 
-const port: number = parseInt(process.env.PORT || "4401", 10);
+app.get("/",(r,s)=>{
+  s.json({message:"welcome to the server"})
+})
+// Start the server
+async function startServer() {
+  try {
+    await sequelize.sync({});
+    console.log("Database connected successfully");
 
-app.listen(port, () => {
-   sequelize
-  .sync({})
-  .then(() => console.log("Database connected successfully"))
-  .catch((err) => {
+    app.listen(port, () => {
+      console.log("Server started");
+    });
+  } catch (err) {
     console.error(err);
-  });
-  console.log("Server started");
-});
+    process.exit(1); // Exit the process with an error code
+  }
+}
+
+startServer();
