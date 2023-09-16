99e14f6178b2af7c280a8118264848f791868af6
isaacolanre <isaacolanre@gmail.com>
2023-09-16 20:03:26 +0100

    added 3 new fields in instituition data

--- interface/institutionData.interfaces.ts
+++ interface/institutionData.interfaces.ts
@@ -3,9 +3,12 @@ interface institutionDataAttributes {
     accountType: string;
     amount: string;
     email: string;
+    examinations: string;
     institutionID: string;
     institutionName: string;
     level: string;
+    paymentPeriod: string;
+    paymentPurpose: string;
     phoneNumber: string;
     studentClass: string;
     studentFullName: string;
